19e78e4b7a2fbc2daf121bac7acbaca100792724
Heather Hartley <hlhartley@gmail.com>
2019-04-16 22:03:09 -0600

    Adds extra spaces

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -8,48 +8,52 @@
 </template>
 
 <script>
-export default {
-  name: "Item",
-  props: ["item"]
-};
+  export default {
+    name: "Item",
+    props: ["item"]
+  };
 </script>
 
 <style scoped>
-.korean-painting {
-  height: 300px;
-}
-.korean-painting-container {
-  font-size: 10px;
-  display: flex;
-  flex-direction: column;
-}
-.overlay {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 100%;
-  width: 100%;
-  opacity: 0;
-  transition: 0.5s ease;
-}
-.korean-painting-container:hover .overlay {
-  opacity: 1;
-}
-.text {
-  background-color: white;
-  opacity: 0.75;
-  color: black;
-  font-size: 12px;
-  position: absolute;
-  bottom: 17px;
-  padding: 7px;
-  left: 50%;
-  -webkit-transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
-  text-align: center;
-  width: 100%;
-}
+  .korean-painting {
+    height: 300px;
+  }
+
+  .korean-painting-container {
+    font-size: 10px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    opacity: 0;
+    transition: 0.5s ease;
+  }
+
+  .korean-painting-container:hover .overlay {
+    opacity: 1;
+  }
+
+  .text {
+    background-color: white;
+    opacity: 0.75;
+    color: black;
+    font-size: 12px;
+    position: absolute;
+    bottom: 17px;
+    padding: 7px;
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    text-align: center;
+    width: 100%;
+  }
 </style>
