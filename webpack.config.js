cd415bcabc604bdc1305a514544cf831c102b6f5
Heather Hartley <hlhartley@gmail.com>
2019-04-17 17:34:01 -0600

    Changes webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     entry: "./src/main.js",//path relative to this file
-    performance: { hints: false },
+    performance: process.env.NODE_ENV === 'production' ? "warning" : false,
     maxEntrypointSize: 512000,
     maxAssetSize: 512000
 }
