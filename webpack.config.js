26c8cdd205899a05dfdf3a53d2b9a218e02e9da5
Heather Hartley <hlhartley@gmail.com>
2019-04-17 15:51:26 -0600

    Fixes asset size

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 
 module.exports = {
     entry: "./src/main.js",//path relative to this file
-    performance: { hints: false }
+    performance: { hints: false },
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
 }
 
