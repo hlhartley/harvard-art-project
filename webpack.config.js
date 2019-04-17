843e05e4724d1d38ff7dd68098393726962b3478
Heather Hartley <hlhartley@gmail.com>
2019-04-17 17:41:35 -0600

    Adds optimization

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,11 @@
 module.exports = {
     performance: process.env.NODE_ENV === 'production' ? "warning" : false,
     maxEntrypointSize: 512000,
-    maxAssetSize: 512000
+    maxAssetSize: 512000,
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
 }
 
